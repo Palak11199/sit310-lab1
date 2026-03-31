a0c71ebbcfa69a05fb68bbbdc9960b1f8e7a5d24
Palak11199 <s225169832@deakin.edu.au>
2026-03-31 12:24:23 +1100

    clean

--- packages/square_turtle_pkg/src/square_turtle.py
+++ packages/square_turtle_pkg/src/square_turtle.py
@@ -1,42 +1,68 @@
 #!/usr/bin/env python3
 
-# Import Dependencies
-import rospy 
-from geometry_msgs.msg import Twist 
-import time 
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import time
+import math
 
-def move_turtle_square(): 
+# -------- Callback function --------
+def pose_callback(msg):
+    rospy.loginfo("X: %.2f | Y: %.2f | Theta: %.2f", msg.x, msg.y, msg.theta)
+
+
+def move_turtle_square():
     rospy.init_node('turtlesim_square_node', anonymous=True)
-    
-    # Init publisher
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) 
+
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+
     rospy.loginfo("Turtles are great at drawing squares!")
 
-    ########## YOUR CODE GOES HERE ##########
+    rate = rospy.Rate(20)
+
     while not rospy.is_shutdown():
-        
-        rate = rospy.Rate(1)  # 1 Hz, adjust as needed
-        
-        # Create a Twist message for moving forward 
-        cmd_vel_msg = Twist() 
-        cmd_vel_msg.linear.x = 2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
 
-        rate.sleep()
+        for i in range(4):
 
-        # Create a Twist message for moving backward 
-        cmd_vel_msg = Twist()
-        cmd_vel_msg.linear.x = -2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
+            # -------- Move forward --------
+            move_cmd = Twist()
+            move_cmd.linear.x = 2.0
 
-        rate.sleep()
+            t0 = rospy.Time.now().to_sec()
+            duration = 2  # adjust for side length
 
-        ###########################################
+            while (rospy.Time.now().to_sec() - t0) < duration:
+                velocity_publisher.publish(move_cmd)
+                rate.sleep()
 
-if __name__ == '__main__': 
+            # Stop
+            velocity_publisher.publish(Twist())
+            time.sleep(0.5)
 
-    try: 
-        move_turtle_square() 
-    except rospy.ROSInterruptException: 
+            # -------- Rotate 90 degrees --------
+            turn_cmd = Twist()
+            angular_speed = math.radians(90)   # 90 deg/sec → 1.57 rad/s
+            angle = math.radians(90)           # target angle
+
+            turn_cmd.angular.z = angular_speed
+
+            t0 = rospy.Time.now().to_sec()
+            current_angle = 0
+
+            while current_angle < angle:
+                velocity_publisher.publish(turn_cmd)
+                t1 = rospy.Time.now().to_sec()
+                current_angle = angular_speed * (t1 - t0)
+                rate.sleep()
+
+            # Stop rotation
+            velocity_publisher.publish(Twist())
+            time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    try:
+        move_turtle_square()
+    except rospy.ROSInterruptException:
         pass
-        
