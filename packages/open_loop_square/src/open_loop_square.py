5db14df114827cffbeac5b49a9591502e8d81d77
Palak11199 <s225169832@deakin.edu.au>
2026-04-28 12:31:41 +1000

    inittial

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -2,65 +2,81 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
- 
-class Drive_Square:
+
+
+class DriveSquare:
     def __init__(self):
-        #Initialize global class variables
-        self.cmd_msg = Twist2DStamped()
 
-        #Initialize ROS node
-        rospy.init_node('drive_square_node', anonymous=True)
-        
-        #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
-        
-    # robot only moves when lane following is selected on the duckiebot joystick app
-    def fsm_callback(self, msg):
-        rospy.loginfo("State: %s", msg.state)
-        if msg.state == "NORMAL_JOYSTICK_CONTROL":
-            self.stop_robot()
-        elif msg.state == "LANE_FOLLOWING":            
-            rospy.sleep(1) # Wait for a sec for the node to be ready
-            self.move_robot()
- 
-    # Sends zero velocities to stop the robot
+        # Get vehicle name
+        self.veh = rospy.get_param("~veh")
+
+        # Initialize ROS node
+        rospy.init_node('open_loop_square', anonymous=True)
+
+        # Publisher
+        self.pub = rospy.Publisher(
+            f'/{self.veh}/car_cmd_switch_node/cmd',
+            Twist2DStamped,
+            queue_size=1
+        )
+
+        rospy.loginfo("Open Loop Square Node Initialized")
+
+        # Give ROS time to connect publisher
+        rospy.sleep(1.0)
+
+        # Start driving immediately (NO FSM)
+        self.move_robot()
+
+    # Stop robot
     def stop_robot(self):
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.0
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
- 
-    # Spin forever but listen to message callbacks
-    def run(self):
-    	rospy.spin() # keeps node from exiting until node has shutdown
 
-    # Robot drives in a square and then stops
+        msg = Twist2DStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.v = 0.0
+        msg.omega = 0.0
+
+        self.pub.publish(msg)
+        rospy.loginfo("Robot Stopped")
+
+    # Drive square pattern
     def move_robot(self):
 
-        #YOUR CODE GOES HERE#
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Forward!")
-        rospy.sleep(1) # straight line driving time
-        
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Backward!")
-        rospy.sleep(1) # straight line driving time
-        
-        ######################
-                
+        rospy.loginfo("Starting Square Movement (NO FSM)")
+
+        msg = Twist2DStamped()
+
+        for i in range(4):
+
+            # Move forward
+            msg.header.stamp = rospy.Time.now()
+            msg.v = 0.3
+            msg.omega = 0.0
+            self.pub.publish(msg)
+
+            rospy.loginfo("Forward")
+            rospy.sleep(2.0)
+
+            # Turn 90 degrees
+            msg.header.stamp = rospy.Time.now()
+            msg.v = 0.0
+            msg.omega = 2.0
+            self.pub.publish(msg)
+
+            rospy.loginfo("Turning")
+            rospy.sleep(1.2)
+
+        # Stop at end
         self.stop_robot()
+
+    def run(self):
+        rospy.spin()
+
 
 if __name__ == '__main__':
     try:
-        duckiebot_movement = Drive_Square()
-        duckiebot_movement.run()
+        node = DriveSquare()
+        node.run()
+
     except rospy.ROSInterruptException:
         pass
